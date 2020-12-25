85255a511ce1d058fa35b9c05a3d959d7c674cee
Minas <whiplashoo721@gmail.com>
2020-12-25 23:07:33 +0200

    reformat

--- day10.py
+++ day10.py
@@ -3,14 +3,15 @@ def part_one(nums):
     three_jolts = 0
     joltages = sorted(nums)
     for i in range(len(joltages) - 1):
-        if joltages[i+1] - joltages[i] == 1:
+        if joltages[i + 1] - joltages[i] == 1:
             one_jolts += 1
-        elif joltages[i+1] - joltages[i] == 3:
+        elif joltages[i + 1] - joltages[i] == 3:
             three_jolts += 1
-    one_jolts += 1    
-    three_jolts += 1    
+    one_jolts += 1
+    three_jolts += 1
     print(one_jolts, three_jolts)
     return one_jolts * three_jolts
+
 
 def part_two(nums):
     pass
@@ -21,4 +22,4 @@ with open("day10.txt") as f:
     for line in f:
         nums.append(int(line.strip()))
     print(part_one(nums))
-    #print(part_two(nums))
+    # print(part_two(nums))
