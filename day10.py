d258bb00029eaf6dd026362563408390cae97ebc
Minas <whiplashoo721@gmail.com>
2020-12-25 23:05:17 +0200

    day10_part1

---
+++ day10.py
@@ -0,0 +1,24 @@
+def part_one(nums):
+    one_jolts = 0
+    three_jolts = 0
+    joltages = sorted(nums)
+    for i in range(len(joltages) - 1):
+        if joltages[i+1] - joltages[i] == 1:
+            one_jolts += 1
+        elif joltages[i+1] - joltages[i] == 3:
+            three_jolts += 1
+    one_jolts += 1    
+    three_jolts += 1    
+    print(one_jolts, three_jolts)
+    return one_jolts * three_jolts
+
+def part_two(nums):
+    pass
+
+
+with open("day10.txt") as f:
+    nums = []
+    for line in f:
+        nums.append(int(line.strip()))
+    print(part_one(nums))
+    #print(part_two(nums))
