76aecb3a62815641ef87e86ba2a46ddc92673245
Minas <whiplashoo721@gmail.com>
2020-12-26 22:35:24 +0200

    day11_part2

--- day11.py
+++ day11.py
@@ -1,6 +1,6 @@
 from copy import deepcopy
 
-adjacent = [(-1, -1), (0, -1), (1, -1), (-1, 0),
+directions = [(-1, -1), (0, -1), (1, -1), (-1, 0),
             (1, 0), (-1, 1), (0, 1), (1, 1)]
 
 
@@ -11,18 +11,16 @@ def print_matrix(matrix):
 
 def part_one(seats):
     stabilized = True
-    # print_matrix(seats)
-    # print()
     next_seats = deepcopy(seats)
     for row in range(len(seats)):
         for col in range(len(seats[row])):
             seat = seats[row][col]
             if seat == "L":
                 should_become_occupied = True
-                for adj in adjacent:
+                for direction in directions:
                     try:
-                        if row + adj[0] >= 0 and col + adj[1] >= 0:
-                            if seats[row + adj[0]][col + adj[1]] == '#':
+                        if row + direction[0] >= 0 and col + direction[1] >= 0:
+                            if seats[row + direction[0]][col + direction[1]] == '#':
                                 should_become_occupied = False
                     except IndexError:
                         continue
@@ -31,10 +29,10 @@ def part_one(seats):
                     next_seats[row][col] = '#'
             if seat == '#':
                 occupied_neighbors = 0
-                for adj in adjacent:
+                for direction in directions:
                     try:
-                        if row + adj[0] >= 0 and col + adj[1] >= 0:
-                            if seats[row + adj[0]][col + adj[1]] == '#':
+                        if row + direction[0] >= 0 and col + direction[1] >= 0:
+                            if seats[row + direction[0]][col + direction[1]] == '#':
                                 occupied_neighbors += 1
                     except IndexError:
                         continue
@@ -52,8 +50,63 @@ def part_one(seats):
         return total
 
 
-def part_two(nums):
-    pass
+def part_two(seats):
+    stabilized = True
+    next_seats = deepcopy(seats)
+    rows_total = len(seats)
+    cols_total = len(seats[0])
+    for row in range(rows_total):
+        for col in range(cols_total):
+            seat = seats[row][col]
+            if seat == "L":
+                should_become_occupied = True
+                for direction in directions:
+                    ptr_row = row
+                    ptr_col = col
+                    while ptr_row >= 0 and ptr_col >= 0 and ptr_row < rows_total and ptr_col < cols_total:
+                        try:
+                            if ptr_row + direction[0] >= 0 and ptr_col + direction[1] >= 0:
+                                if seats[ptr_row + direction[0]][ptr_col + direction[1]] == 'L':
+                                    break
+                                if seats[ptr_row + direction[0]][ptr_col + direction[1]] == '#':
+                                    should_become_occupied = False
+                                    break
+                            ptr_row += direction[0]
+                            ptr_col += direction[1]
+                        except IndexError:
+                            break
+                if should_become_occupied:
+                    stabilized = False
+                    next_seats[row][col] = '#'
+            if seat == '#':
+                occupied_neighbors = 0
+                for direction in directions:
+                    ptr_row = row
+                    ptr_col = col
+                    while ptr_row >= 0 and ptr_col >= 0 and ptr_row < rows_total and ptr_col < cols_total:
+                        try:
+                            if ptr_row + direction[0] >= 0 and ptr_col + direction[1] >= 0:
+                                if seats[ptr_row + direction[0]][ptr_col + direction[1]] == 'L':
+                                    break
+                                if seats[ptr_row + direction[0]][ptr_col + direction[1]] == '#':
+                                    occupied_neighbors += 1
+                                    break
+                            ptr_row += direction[0]
+                            ptr_col += direction[1]
+                        except IndexError:
+                            break
+                if occupied_neighbors >= 5:
+                    stabilized = False
+                    next_seats[row][col] = 'L'
+    if not stabilized:
+        return part_two(next_seats)
+    else:
+        total = 0
+        for row in next_seats:
+            for seat in row:
+                if seat == "#":
+                    total += 1
+        return total
 
 
 with open("day11.txt") as f:
@@ -61,4 +114,4 @@ with open("day11.txt") as f:
     for line in f:
         seats.append([char for char in line.strip()])
     print(part_one(seats))
-    # print(part_two(nums))
+    print(part_two(seats))
