f25870bd89061fa5c6a7453c50ac5f4b2dad61af
Minas <whiplashoo721@gmail.com>
2020-12-26 23:33:15 +0200

    day12_part2

--- day12.py
+++ day12.py
@@ -40,7 +40,29 @@ def part_one(instructions):
 
 
 def part_two(instructions):
-    pass
+    x = 0
+    y = 0
+    wx = 10
+    wy = 1
+    for inst in instructions:
+        cmd = inst[0]
+        val = int(re.findall(r'\d+', inst)[0])
+        if cmd in directions:
+            wx, wy = move(cmd, wx, wy, val)
+        if cmd == 'F':
+            for i in range(val):
+                x += wx
+                y += wy
+        elif cmd == 'R':
+            val /= 90
+            for i in range(int(val)):
+                wx, wy = wy, -wx
+        elif cmd == 'L':
+            val /= 90
+            for i in range(int(val)):
+                wx, wy = -wy, wx
+    distance = abs(x) + abs(y)
+    return distance
 
 
 with open("day12.txt") as f:
