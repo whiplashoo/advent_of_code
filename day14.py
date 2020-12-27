f4caa749af05b07f71ffec5f624422d338527896
Minas <whiplashoo721@gmail.com>
2020-12-27 20:14:19 +0200

    day14_part2

--- day14.py
+++ day14.py
@@ -1,4 +1,4 @@
-def apply_mask(val, mask):
+def apply_mask1(val, mask):
     retval = ''
     for v, m in zip(val, mask):
         if m == '0':
@@ -8,6 +8,34 @@ def apply_mask(val, mask):
         else:
             retval += v
     return int(retval, 2)
+
+
+def apply_mask2(pos, mask):
+    x_count = mask.count('X')
+    possible_combinations = 2 ** x_count
+    ret_arr = [[] for _ in range(possible_combinations)]
+    consecutive = 2 ** (x_count - 1)
+    for p, m in zip(pos, mask):
+        if m == '0':
+            for s in ret_arr:
+                s.append(p)
+        elif m == '1':
+            for s in ret_arr:
+                s.append('1')
+        else:
+            wrote = 0
+            write_zeros = True
+            for s in ret_arr:
+                if wrote == consecutive:
+                    wrote = 0
+                    write_zeros = not write_zeros
+                if write_zeros:
+                    s.append('0')
+                else:
+                    s.append('1')
+                wrote += 1
+            consecutive /= 2
+    return [int(''.join(s), 2) for s in ret_arr]
 
 
 def part_one(inp):
@@ -20,15 +48,27 @@ def part_one(inp):
             pos = line.split('[')[1].split(']')[0]
             val = line.split('=')[1][1:]
             val = bin(int(val))[2:].zfill(36)
-            mem[pos] = apply_mask(val, mask)
+            mem[pos] = apply_mask1(val, mask)
     return (sum(mem.values()))
 
 
-def part_two(ids):
-    pass
+def part_two(inp):
+    mem = {}
+    for line in inp:
+        line = line.strip()
+        if 'mask' in line:
+            mask = line.split('=')[1][1:]
+        else:
+            pos = line.split('[')[1].split(']')[0]
+            val = line.split('=')[1][1:]
+            pos = bin(int(pos))[2:].zfill(36)
+            addresses_to_change = apply_mask2(pos, mask)
+            for address in addresses_to_change:
+                mem[address] = int(val)
+    return (sum(mem.values()))
 
 
 with open("day14.txt") as f:
     inp = f.readlines()
     print(part_one(inp))
-    # print(part_two(inp[1].split(',')))
+    print(part_two(inp))
