6204b9671b7c1b1b81df8530e4f27862fa5a8280
Minas Giannekas <whiplashoo721@gmail.com>
2021-12-05 18:07:22 +0200

    day5 simplify

--- 2021/day5.py
+++ 2021/day5.py
@@ -12,58 +12,35 @@ for line in inp:
     max_x = max(max_x, r[0], r[2])
     max_y = max(max_y, r[1], r[3])
 
-d = [[0 for col in range(max_x + 1)] for row in range(max_y + 1)]
+d = [[0 for col in range(max_x + 2)] for row in range(max_y + 2)]
 
+print(max_x, max_y)
 overlaps = 0
 for v in vents:
     x1 = v[0][0]
     x2 = v[1][0]
     y1 = v[0][1]
     y2 = v[1][1]
-    # horizontal: x1 = x2
-    if x1 == x2:
-        x = x1
-        # if y1 < y2
-        if y1 < y2:
-            for i in range(y1, y2 + 1):
-                d[i][x] += 1
-        # if y1 > y2
-        if y1 > y2:
-            for i in range(y2, y1 + 1):
-                d[i][x] += 1
-    # vertical: y1 = y2
-    elif y1 == y2:
-        y = y1
-        # if x1 < x2
-        if x1 < x2:
-            for i in range(x1, x2 + 1):
-                d[y][i] += 1
-        # if x1 > x2
-        if x1 > x2:
-            for i in range(x2, x1 + 1):
-                d[y][i] += 1
-    # diagonal: abs(y1 - y2) == abs(x1 - x2)
-    elif abs(y1 - y2) == abs(x1 - x2):
-        # e.g. (1,1) -> (3,3)
-        if x1 < x2 and y1 < y2:
-            diff = x2 - x1
-            for i in range(diff + 1):
-                d[y1 + i][x1 + i] += 1
-        # e.g. (3,3) -> (1,1)
-        if x1 > x2 and y1 > y2:
-            diff = x1 - x2
-            for i in range(diff + 1):
-                d[y2 + i][x2 + i] += 1
-        # e.g. (7,9) -> (9,7)
-        if x1 < x2 and y1 > y2:
-            diff = x2 - x1
-            for i in range(diff + 1):
-                d[y1 - i][x1 + i] += 1
-        # e.g. (9,7) -> (7,9)
-        if x1 > x2 and y1 < y2:
-            diff = x1 - x2
-            for i in range(diff + 1):
-                d[y1 + i][x1 - i] += 1
+
+    dx = x2 - x1
+    dy = y2 - y1
+
+    if dx > 0:
+        dx = 1
+    elif dx < 0:
+        dx = -1
+    if dy > 0:
+        dy = 1
+    elif dy < 0:
+        dy = -1
+
+    x, y = v[0]
+    while (x, y) != v[1]:
+        print(x, y)
+        d[x][y] += 1
+        x += dx
+        y += dy
+    d[x2][y2] += 1
 
 for i in range(len(d)):
     for j in range(len(d[0])):
