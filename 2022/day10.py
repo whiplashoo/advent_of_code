2c2d51a5758eaae6ee1b4822c9dec15187248c90
minas-mbp <whiplashoo721@gmail.com>
2022-12-11 21:06:00 +0100

    day10 part 2 done

--- 2022/day10.py
+++ 2022/day10.py
@@ -1,24 +1,40 @@
-from aoc import input_as_lines
+from aoc import input_as_lines, print_matrix
 
 inp = input_as_lines("day10.txt")
 
 X = 1
 ticks = 0
 ss = {}
-
+xv = {}
+# Part 1
 for line in inp:
     line = line.split()
     if line[0] == "addx":
         ticks += 1
         ss[ticks] = X * ticks
+        xv[ticks] = X
         ticks += 1
         ss[ticks] = X * ticks
+        xv[ticks] = X
         X += int(line[1])
     else:
         ticks += 1
         ss[ticks] = X * ticks
+        xv[ticks] = X
 print(ss)
+print(xv)
 ret = 0
 for i in range(20, ticks + 1, 40):
     ret += ss[i]
 print(f"ret: {ret}")
+
+# PART 2
+crt = [["." for _ in range(40)] for y in range(6)]
+for t in range(1, ticks):
+    row = t // 40
+    col = (t-1) % 40
+    sprite_col = [(xv[t] % 40) - 1, (xv[t] % 40), (xv[t] % 40) + 1]
+    if col in sprite_col:
+        crt[row][col] = "#"
+
+print_matrix(crt)
