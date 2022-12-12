925fe8234da50ddfdb7d8e9763bf49955ec1493a
Minas Giannekas <whiplashoo721@gmail.com>
2022-12-12 23:00:00 +0200

    day 11 part 2 done

--- 2022/day11.py
+++ 2022/day11.py
@@ -4,7 +4,7 @@ from collections import defaultdict
 
 from aoc import input_as_lines, parse_positive_ints_str
 
-inp = input_as_lines("day11t.txt")
+inp = input_as_lines("day11.txt")
 
 monkeys = []
 
@@ -29,6 +29,7 @@ def apply_operation(operation, operand, input):
         return input * operand
 
 
+lcm = 1
 # PART 1
 for line in inp:
     if line == "":
@@ -45,6 +46,7 @@ for line in inp:
         monkeys[-1].op = (operator, operand)
     elif "Test" in line:
         monkeys[-1].divisible = parse_positive_ints_str(line)[0]
+        lcm *= parse_positive_ints_str(line)[0]
     elif "true" in line:
         monkeys[-1].t = parse_positive_ints_str(line)[0]
     elif "false" in line:
@@ -55,28 +57,13 @@ while rounds < 10000:
     for m in monkeys:
         for worry in m.items:
             inspected[monkeys.index(m)] += 1
-            st = time.perf_counter()
             worry = apply_operation(
                 m.op[0], worry if m.op[1] == "self" else m.op[1], worry)
-            end = time.perf_counter()
-            #print(f"operation takes {end - st}")
-            #worry = math.floor(worry/3)
-            st = time.perf_counter()
-            print(worry)
+            worry %= lcm
             if worry % m.divisible == 0:
-                end = time.perf_counter()
-                #print(f"modulo takes {end - st}")
-                st = time.perf_counter()
                 monkeys[m.t].items.append(worry)
-                end = time.perf_counter()
-                print(f"appending takes {end - st}")
             else:
-                end = time.perf_counter()
-                st = time.perf_counter()
-                #print(f"modulo takes {end - st}")
                 monkeys[m.f].items.append(worry)
-                end = time.perf_counter()
-                print(f"appending takes {end - st}")
         m.items = []
     rounds += 1
     print(rounds)
