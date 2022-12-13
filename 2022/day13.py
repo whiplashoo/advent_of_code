204bcc69674d22e698fa7f250a9a008e111800b2
Minas Giannekas <whiplashoo721@gmail.com>
2022-12-13 16:50:01 +0200

    day13 part 2 done

--- 2022/day13.py
+++ 2022/day13.py
@@ -1,13 +1,16 @@
 
+from functools import cmp_to_key
+
 from aoc import input_as_lines
 
 inp = input_as_lines("day13.txt")
 
 inp.append("")
 
+# Eval to the rescue!
+
 
 def parse_packet(line):
-    print(line)
     p = eval(line)
     return p
 
@@ -16,16 +19,16 @@ def compare(left, right):
     n = max(len(left), len(right))
     for i in range(n):
         if i >= len(left):
-            return True
+            return 1
         if i >= len(right):
-            return False
+            return -1
         left_val = left[i]
         right_val = right[i]
         if isinstance(left_val, int) and isinstance(right_val, int):
             if left_val < right_val:
-                return True
+                return 1
             if left_val > right_val:
-                return False
+                return -1
         else:
             if isinstance(left_val, int):
                 left_val = [left_val]
@@ -36,6 +39,7 @@ def compare(left, right):
                 return c
 
 
+# PART 1
 packets = []
 ret = 0
 idx = 1
@@ -49,4 +53,13 @@ for line in inp:
         continue
     print("------------------")
     packets.append(parse_packet(line))
-print(ret)
+
+# PART 2
+packets = [[[2]], [[6]]]
+for line in inp:
+    if line == "":
+        continue
+    packets.append(parse_packet(line))
+
+packets.sort(key=cmp_to_key(compare), reverse=True)
+print((packets.index([[2]]) + 1) * (packets.index([[6]]) + 1))
