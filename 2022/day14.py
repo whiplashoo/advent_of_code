f0045a132bcb7b0507160cd9b010af2aa1796547
minas-mbp <whiplashoo721@gmail.com>
2022-12-14 14:56:33 +0200

    day14 part 2 done, refactor part 1 to use dict

--- 2022/day14.py
+++ 2022/day14.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+from copy import deepcopy
+
 from aoc import input_as_lines, print_matrix
 
 inp = input_as_lines("day14.txt")
@@ -18,10 +21,9 @@ for line in inp:
         path.append((int(p[0]), int(p[1])))
     paths.append(path)
 
-min_y = 0
 COLS = max_x - min_x + 1
 ROWS = max_y + 1
-cave = [["." for col in range(COLS)] for row in range(ROWS)]
+inf_cave = defaultdict(lambda: ".")
 
 for path in paths:
     for i in range(len(path) - 1):
@@ -35,30 +37,86 @@ for path in paths:
         endy = max(cy, py)
         for yy in range(starty, endy + 1):
             for xx in range(startx, endx + 1):
-                cave[yy][xx] = "#"
+                inf_cave[(yy, xx)] = "#"
 
-# PART 1
-scol, srow = (500 - min_x, 0)
-ret = 0
-reached_abyss = False
-while not reached_abyss:
-    while True:
-        ncol, nrow = scol, srow + 1
-        if nrow >= ROWS:
-            reached_abyss = True
-            break
-        if cave[nrow][ncol] == ".":
-            srow = nrow
-        elif cave[nrow][ncol] == "o" or cave[nrow][ncol] == "#":
-            if cave[nrow][ncol - 1] == ".":
-                srow, scol = nrow, ncol - 1
-            elif cave[nrow][ncol + 1] == ".":
-                srow, scol = nrow, ncol + 1
-            else:
-                cave[srow][scol] = "o"
+
+def print_inf_cave(inf_cave, max_y):
+    """
+    Helper to convert a dict cave to a grid and print it
+    """
+    min_x = 1000
+    max_x = -1000
+    for point in inf_cave.keys():
+        row, col = point
+        min_x = min(col, min_x)
+        max_x = max(col, max_x)
+    COLS = max_x - min_x + 3
+    ROWS = max_y + 2
+    cave = [["." for col in range(COLS)] for row in range(ROWS)]
+    for point in inf_cave.keys():
+        row, col = point
+        cave[row][col-min_x] = inf_cave[point]
+    print("\n")
+    print_matrix(cave)
+
+
+def solve1(cave):
+    scol, srow = (500 - min_x, 0)
+    ret = 0
+    reached_abyss = False
+    while not reached_abyss:
+        while True:
+            ncol, nrow = scol, srow + 1
+            if nrow >= ROWS:
+                reached_abyss = True
+                break
+            if cave[(nrow, ncol)] == ".":
+                srow = nrow
+            elif cave[(nrow, ncol)] == "o" or cave[(nrow, ncol)] == "#":
+                if cave[(nrow, ncol - 1)] == ".":
+                    srow, scol = nrow, ncol - 1
+                elif cave[(nrow, ncol + 1)] == ".":
+                    srow, scol = nrow, ncol + 1
+                else:
+                    cave[(srow, scol)] = "o"
+                    scol, srow = (500 - min_x, 0)
+                    ret += 1
+                    break
+    return ret
+
+
+def solve2(cave):
+    floor_y = max_y + 2
+    scol, srow = (500 - min_x, 0)
+    ret = 0
+    filled = False
+    while not filled:
+        while True:
+            ncol, nrow = scol, srow + 1
+            if nrow == 1 and cave[(nrow, ncol - 1)] == "o" and cave[(nrow, ncol + 1)] == "o" and cave[(nrow, ncol)] == "o":
+                ret += 1
+                filled = True
+                break
+            if nrow == floor_y:
+                cave[(srow, scol)] = "o"
                 scol, srow = (500 - min_x, 0)
                 ret += 1
                 break
+            elif cave[(nrow, ncol)] == ".":
+                srow = nrow
+            elif cave[(nrow, ncol)] == "o" or cave[(nrow, ncol)] == "#":
+                if cave[(nrow, ncol - 1)] == ".":
+                    srow, scol = nrow, ncol - 1
+                elif cave[(nrow, ncol + 1)] == ".":
+                    srow, scol = nrow, ncol + 1
+                else:
+                    cave[(srow, scol)] = "o"
+                    scol, srow = (500 - min_x, 0)
+                    ret += 1
+                    break
+
+    return ret
 
 
-print(ret)
+print(solve1(deepcopy(inf_cave)))
+print(solve2(inf_cave))
