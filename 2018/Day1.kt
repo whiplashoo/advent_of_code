61c3b6f304f058800284f4b456da531ea7682359
minas-mbp <whiplashoo721@gmail.com>
2023-10-09 23:20:19 +0300

    day01 2018 in Kotlin

---
+++ 2018/Day1.kt
@@ -0,0 +1,28 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        return input.sumOf {
+            it.toInt()
+        }
+    }
+
+    fun part2(input: List<String>): Int {
+        var i = 0
+        var freq = 0
+        val frequencies = mutableListOf<Int>()
+        do {
+            freq += input[i].toInt()
+            if (freq in frequencies) {
+                return freq
+            }
+            frequencies.add(freq)
+            i++
+            if (i == input.size) {
+                i = 0
+            }
+        } while (true)
+    }
+
+    val input = readInput("Day01")
+    part1(input).println()
+    part2(input).println()
+}
