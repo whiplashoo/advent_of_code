f36f00b63e443af98142385277898dfa9c2bf0e1
Minas Giannekas <whiplashoo721@gmail.com>
2023-12-01 08:55:25 +0200

    day1 part 1 done

--- 2023/day1.py
+++ 2023/day1.py
@@ -1,17 +1,24 @@
-from aoc import input_as_lines
+import re
+
+from aoc import input_as_lines, parse_positive_ints_str
 
 inp = input_as_lines("day1.txt")
-cals = []
+sum = 0
+
+digits = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 
 # PART 1
-bucket = 0
-for i in inp:
-    if i == "":
-        cals.append(bucket)
-        bucket = 0
-    else:
-        bucket += int(i.rstrip('\n'))
-print(max(cals))
+for line in inp:
+    ints = re.findall(r'[0-9]', line)
+    first = ints[0]
+    second = ints[-1]
+    print(line + " " + str(int(str(first) + "" + str(second))))
+    sum += int(ints[0] + ints[-1])
+print(sum)
 
+sum = 0
+pos = {}
 # PART 2
-print(sum(sorted(cals, reverse=True)[:3]))
+
+            
+print(sum)
