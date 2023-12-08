78ee2e30c533645d8248e9faedeea037ba9589de
Minas Giannekas <whiplashoo721@gmail.com>
2023-12-08 09:22:33 +0200

    day 8 part 1 done

---
+++ 2023/day8.py
@@ -0,0 +1,25 @@
+from aoc import input_as_lines
+
+inp = input_as_lines("day8.txt")
+
+dirs = inp[0]
+
+nodes = {}
+
+for line in inp[2:]:
+    line = line.replace(" = (", " ").replace(",", "").replace(")", "").split(" ")
+    nodes[line[0]] = (line[1], line[2])
+
+steps = 0
+cur = "AAA"
+cur_inst = 0
+while cur!= "ZZZ":
+    inst = dirs[cur_inst % len(dirs)]
+    if inst == "L":
+        cur = nodes[cur][0]
+    else:
+        cur = nodes[cur][1]
+    cur_inst += 1
+    steps += 1
+
+print(steps)
