6f002ee9fcc42b2fe24f636a64d218afaaa67bf9
Minas Giannekas <whiplashoo721@gmail.com>
2023-12-11 10:22:41 +0200

    cleanup

--- 2023/day10.py
+++ 2023/day10.py
@@ -1,6 +1,6 @@
 from aoc import input_as_lines, print_matrix
 
-inp = input_as_lines("day10t.txt")
+inp = input_as_lines("day10.txt")
 
 N = []
 for line in inp:
@@ -64,21 +64,24 @@ def bfs(N, start):
 bfs(N, [get_start(N)])
 
 # PART 2
+# Expand the maze and add padding
 NN = []
+col_len = len(inp[0]) * 2 + 2
+NN.append(["#" for _ in  range(col_len)])
 for line in inp:
-    rr = []
+    rr = ["#"]
     for h in line:
         rr.append(h)
         rr.append("#")
+    rr.append("#")
     NN.append(rr)
-    NN.append(["#" for _ in  range(len(line * 2))])
+    NN.append(["#" for _ in  range(col_len)])
 
-col = 0
-row = 0
 COLS = len(NN[0])
 ROWS = len(NN)
-loop = [(x * 2, y * 2) for (x,y) in loop]
+loop = [(x * 2 + 1, y * 2 + 1) for (x,y) in loop]
 
+# Link the loop's points again
 to_add = []
 for row in range(ROWS):
     for col in range(COLS):
@@ -87,10 +90,8 @@ for row in range(ROWS):
                 link = "-" if m[0] == 0 else "|"
                 NN[row + m[0]][col + m[1]] = link
                 to_add.append((row + m[0], col + m[1]))
-
 for i in to_add:
     loop.append(i)
-print_matrix(NN)
 
 def can_fill(N, new_r, new_c):
     if (new_r < 0 or new_c < 0 or new_r >= len(N) or new_c >= len(N[0])):
@@ -99,9 +100,9 @@ def can_fill(N, new_r, new_c):
         return False
     return True
 
-def flood_fill(N, start):
-    queue = [(p, 0) for p in start]
-    visited = set(start)
+def flood_fill(N, start = (0,0)):
+    queue = [(start, 0) ]
+    visited = {start}
     while queue:
         (v, steps) = queue.pop(0)
         for move in moves:
@@ -109,13 +110,9 @@ def flood_fill(N, start):
             if adj not in visited and can_fill(N, adj[0], adj[1]):
                 visited.add(adj)
                 queue.append((adj, steps + 1))
-                N[adj[0]][adj[1]] = "%"
-        # print_matrix(NN)
-        # print()
-flood_fill(NN, [get_start(NN)])
-print()
-print_matrix(NN)
+                N[adj[0]][adj[1]] = "#"
 
+flood_fill(NN)
 s = 0
 for row in range(ROWS):
     for col in range(COLS):
