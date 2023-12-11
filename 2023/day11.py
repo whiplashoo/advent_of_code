771c6af24fc3c5e07ee9b1783b30279a7e04388d
Minas Giannekas <whiplashoo721@gmail.com>
2023-12-11 18:58:44 +0200

    day11 part1 done

---
+++ 2023/day11.py
@@ -0,0 +1,38 @@
+import itertools
+from copy import deepcopy
+
+from aoc import get_manhattan_distance, input_as_lines
+
+inp = input_as_lines("day11.txt")
+
+galaxies = []
+for row, line in enumerate(inp):
+    for col, c in enumerate(line):
+        if c == "#":
+            galaxies.append([row,col])
+
+print(galaxies)
+
+all_rows = set([g[0] for g in galaxies])
+all_cols = set([g[1] for g in galaxies])
+empty_rows = sorted(set(range(row)).difference(all_rows))
+empty_cols = sorted(set(range(col)).difference(all_cols))
+print(empty_rows)
+print(empty_cols)
+ex_galaxies = deepcopy(galaxies)
+for i, g in enumerate(galaxies):
+    for r in empty_rows:
+        if g[0] > r:
+            ex_galaxies[i][0] += 1
+    for c in empty_cols:
+        if g[1] > c:
+            ex_galaxies[i][1] += 1
+
+print(ex_galaxies)
+
+combos = list(itertools.combinations(ex_galaxies, 2))
+s = 0
+for c in combos:
+    s += get_manhattan_distance(c[0], c[1])
+
+print(s)
