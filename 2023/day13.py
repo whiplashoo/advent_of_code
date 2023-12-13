e83339915c7ecd0939318c89f54bc5dff186ea24
Minas Giannekas <whiplashoo721@gmail.com>
2023-12-13 15:59:43 +0200

    day13 part 1 done

--- 2023/day13.py
+++ 2023/day13.py
@@ -33,7 +33,7 @@ def scan_cols(N):
         if cur_col == next_col:
             left, right = col, col + 1
             while True:
-                if (left == 0 or right == COLS -1) and get_col(N, left) == get_col(N, right):
+                if (left == 0 or right == COLS - 1) and get_col(N, left) == get_col(N, right):
                     return col + 1
                 if get_col(N, left) != get_col(N, right):
                     break
@@ -42,38 +42,44 @@ def scan_cols(N):
                     right += 1
     return 0
 
-s = 0
+def search_p1(N):
+    rs = scan_rows(N)
+    cs = scan_cols(N)
+    return rs + cs
+
+def search_p2(N, prev):
+    for row in range(len(N)):
+        for col in range(len(N[0])):
+            NN = deepcopy(N)
+            NN[row][col] = "#" if NN[row][col] == "." else "."
+            print_matrix(NN)
+            print()
+            rs = scan_rows(NN)
+            cs = scan_cols(NN)
+            if rs != 0 and rs != prev:
+                return rs
+            if cs != 0 and cs != prev:
+                return cs
+    return 0
+
+s1 = 0
+s2 = 0
 for line in inp:
     if line == "":
-        found_new = False
-        for row in len(N):
-            for col in len(N[0]):
-                NN = deepcopy(N)
-                NN[row][col] = "#" if NN[row][col] == "." else "#"
-                rs = scan_rows(NN)
-                cs = scan_cols(NN)
-                if rs != 0 or cs != 0:
-                    print("FOUND NEW REFLECTION")
-                    s += rs + cs
-                    found_new = True
-                    break
-        #s += rs + cs
+        prev = search_p1(N)
+        s1 += prev
+        p2 = search_p2(N, prev)
+        print(prev, p2)
+        s2 += p2
         N = []
     else:
         N.append([h for h in line])
 
-for row in len(N):
-    for col in len(N[0]):
-        NN = deepcopy(N)
-        NN[row][col] = "#" if NN[row][col] == "." else "#"
-        rs = scan_rows(NN)
-        cs = scan_cols(NN)
-        if rs != 0 or cs != 0:
-            print("FOUND NEW REFLECTION")
-            s += rs + cs
-            found_new = True
-            break
+prev = search_p1(N)
+s1 += prev
+print(s1)
+s2 += search_p2(N, prev)
 #s += scan_rows(N) + scan_cols(N)
-print(s)
+print(s2)
 
 
