f2fabbb280409fc0344b12821ef9f40b0bc1eb92
minas-mbp <whiplashoo721@gmail.com>
2024-12-09 11:13:27 +0200

    day9 done

--- 2024/day9.py
+++ 2024/day9.py
@@ -3,7 +3,7 @@ from collections import defaultdict
 from copy import deepcopy
 import time
 
-inp = input_as_string(filename="day9t.txt")
+inp = input_as_string(filename="day9.txt")
 blocks = defaultdict(lambda: ".")
 is_file = True
 i = 0
@@ -30,7 +30,6 @@ right = n - 1
 left = 0
 
 while left < right:
-    print(left, right, blocks[left], blocks[right])
     if blocks[left] == ".":
         while blocks[right] == ".":
             right -= 1
@@ -44,47 +43,11 @@ for i in range(n):
         p1 += i * blocks[i]
 print(p1)
 
-print_block(blocks2)
-
 right = n - 1
 left = 0
 
-# while left < right:
-#     print(left, right, blocks2[left], blocks2[right])
-#     print_block(blocks2)
-#     #time.sleep(0.5)
-#     if blocks2[left] == ".":
-#         left_len = 0
-#         temp_left = left
-#         while blocks2[temp_left] == ".":
-#             left_len += 1
-#             temp_left += 1
-#         while blocks2[right] == ".":
-#             right -= 1
-#         right_len = 0
-#         temp_right = right
-#         while blocks2[temp_right] == blocks2[right]:
-#             temp_right -= 1
-#             right_len += 1
-#         print(f"left_len {left_len}  + right_len {right_len}")
-#         if right_len <= left_len:
-#             for i in range(right_len):
-#                 blocks2[left + i] = blocks2[right - i]
-#                 blocks2[right - i] = "."
-#                 print(f"swapping {left +i} with {right + i}")
-#             left += right_len
-#             right -= right_len
-#         else:
-#             left += right_len
-#             right -= right_len
-#     else:
-#         left += 1
-
-while right > 4:
-    print(left, right, blocks2[left], blocks2[right])
-    print_block(blocks2)
+while right > 0:
     left = 0
-    time.sleep(0.5)
     while blocks2[right] == ".":
         right -= 1
     right_len = 0
@@ -92,23 +55,25 @@ while right > 4:
     while blocks2[temp_right] == blocks2[right]:
         temp_right -= 1
         right_len += 1
-    while left < n - 1:
-        while blocks2[left] != ".":
+    made_swap = False
+    while left <= right and not made_swap:
+        while blocks2[left] != "." and left <= right:
             left += 1
         left_len = 0
         temp_left = left
-        while blocks2[temp_left] == ".":
+        while blocks2[temp_left] == "." and temp_left < right:
             left_len += 1
             temp_left += 1
         if right_len <= left_len:
             for i in range(right_len):
                 blocks2[left + i] = blocks2[right - i]
                 blocks2[right - i] = "."
-                print(f"swapping {left +i} with {right + i}")
-            break
+            made_swap = True
         left += left_len
-        print(left)
     right -= right_len
 
-print_block(blocks2)
-
+p2 = 0
+for i in range(n):
+    if blocks2[i] != ".":
+        p2 += i * blocks2[i]
+print(p2)
