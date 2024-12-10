4a31445857525d16460b7b062200496ee584f8d2
minas-mbp <whiplashoo721@gmail.com>
2024-12-10 10:03:45 +0200

    day 10 part 2

--- 2024/day10.py
+++ 2024/day10.py
@@ -1,4 +1,4 @@
-from aoc import input_as_lines, print_matrix
+from aoc import input_as_lines
 inp = input_as_lines("day10.txt")
 
 N = []
@@ -9,8 +9,6 @@ ROWS = len(N)
 COLS = len(N[0])
 moves = [(-1, 0), (0, -1), (1, 0), (0, 1)]
 
-print_matrix(N)
-
 def can_move(matrix, new_r, new_c, cur_val):
     if (new_r < 0 or new_c < 0 or new_r >= ROWS or new_c >= COLS):
         return False
@@ -19,6 +17,7 @@ def can_move(matrix, new_r, new_c, cur_val):
     return False
 
 p1 = 0
+p2 = 0
 
 def bfs(N, start):
     global p1
@@ -37,11 +36,27 @@ def bfs(N, start):
                 queue.append((adj, steps + 1))
     return None
 
-# # PART 1
+def bfs2(N, start):
+    global p2
+    queue = [(p, 0) for p in start]
+    while queue:
+        (v, steps) = queue.pop(0)
+        cur_val = N[v[0]][v[1]]
+        if steps == 9:
+            p2 += len(queue) + 1
+            return
+        for move in moves:
+            adj = (v[0] + move[0], v[1] + move[1])
+            if can_move(N, adj[0], adj[1], cur_val):
+                queue.append((adj, steps + 1))
+    return None
+
 for row in range(ROWS):
     for col in range(COLS):
         start = N[row][col]
         if start == 0:
             bfs(N, [(row, col)])
+            bfs2(N, [(row, col)])
 
 print(p1)
+print(p2)
