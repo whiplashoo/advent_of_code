db0eee289406782a29f334ec1e5d8dee7ca12a6b
minas-mbp <whiplashoo721@gmail.com>
2024-12-13 18:17:20 +0200

    day 13 both parts

--- 2024/day13.py
+++ 2024/day13.py
@@ -1,15 +1,15 @@
 from aoc import input_as_lines, parse_positive_ints_str
-import sys
-from itertools import product
-
 inp = input_as_lines("day13.txt")
-combs = list(product(range(101), repeat = 2))
 
 def solve(A, B, P):
-    cost = sys.maxsize
-    found = False
-    bb = ((A[0] * P[1]) - (A[1] * P[0])) /  (A[0] * B[1]) - (A[1] * B[0]) 
-    print(bb)
+    P[1] += 10000000000000
+    P[0] += 10000000000000
+    bb = ((A[0] * P[1]) - (A[1] * P[0])) /  ((A[0] * B[1]) - (A[1] * B[0]))
+    if bb > 0 and bb.is_integer():
+        aa = (P[1] - B[1] * bb) / A[1]
+        if aa.is_integer():
+            return (aa * 3) + bb
+    return 0
 
 sum = 0
 for i in range(0, len(inp), 4):
@@ -18,9 +18,3 @@ for i in range(0, len(inp), 4):
     P = parse_positive_ints_str(inp[i + 2])
     sum += solve(A,B,P)
 print(sum)
-
-# a * 94 + b * 22 = 8400
-# a * 34 + b * 67 = 5400
-# a = (8400 - 22b) / 94
-# a = (5400 - 67b) / 34
-# (8400 - 22b) / 94 = (5400 - 67b) / 34
