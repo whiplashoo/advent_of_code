25407635a22af34a6f9075dc6781710f59f830f3
minas-mbp <whiplashoo721@gmail.com>
2025-12-10 17:51:53 +0200

    day 7 part 2 attempt

--- 2025/day7.py
+++ 2025/day7.py
@@ -1,6 +1,6 @@
 from aoc import input_as_lines, print_matrix
 
-inp = input_as_lines("day7.txt")
+inp = input_as_lines("day7t.txt")
 
 p1 = 0
 N = []
@@ -24,3 +24,27 @@ for r in range(ROWS - 1):
 			beams.append((r + 1, c))
 
 print(p1)
+
+# PART 2
+# need dynamic programming
+
+univs = [[(0, inp[0].index("S"))]]
+
+for r in range(3):#ROWS - 1):
+	for univ in univs:
+		print(univs)
+		for c in range(COLS):
+			a = N[r][c]
+			if (r,c) in univ and N[r+1][c] == "^":
+				if c + 1 < COLS:
+					new_univ = univ + [(r + 1, c + 1)]
+					univs.append(new_univ)
+				if c - 1 >= 0:
+					new_univ = univ + [(r + 1, c - 1)]
+					univs.append(new_univ)
+			elif (r,c) in univ and N[r+1][c] == ".":
+				univ.append((r + 1, c))
+				print(univ)
+	print(univs)
+
+print(len(univs))
