2202d7fa7323a47a929c44fedabf43512bc50cad
minas-mbp <whiplashoo721@gmail.com>
2025-12-10 10:28:52 +0200

    day 6 part 2

--- 2025/day6.py
+++ 2025/day6.py
@@ -1,6 +1,7 @@
-from aoc import input_as_lines, parse_ints_str
+from aoc import input_as_lines, parse_ints_str, print_matrix
+from functools import reduce
 
-inp = input_as_lines("day6t.txt")
+inp = input_as_lines("day6.txt")
 
 got_symbols = False
 s = []
@@ -8,7 +9,6 @@ s = []
 p1 = []
 
 for line in inp[::-1]:
-	print(line)
 	if not got_symbols:
 		s = line.split()
 		for i in range(len(s)):
@@ -16,7 +16,6 @@ for line in inp[::-1]:
 		got_symbols = True
 	else:
 		nums = parse_ints_str(line)
-		print(nums)
 		for idx, num in enumerate(nums):
 			if s[idx] == "*":
 				p1[idx] *= nums[idx]
@@ -25,22 +24,36 @@ for line in inp[::-1]:
 
 print(sum(p1))
 
-p2 = []
-got_symbols = False
-s = []
-			
-for line in inp[::-1]:
-	print(line)
-	if not got_symbols:
-		s = line.split()
-		for i in range(len(s)):
-			p1.append(0 if s[i] == "+" else 1)
-		got_symbols = True
-	else:
-		nums = parse_ints_str(line)
-		print(nums)
-		for idx, num in enumerate(nums):
-			if s[idx] == "*":
-				p1[idx] *= nums[idx]
-			else:
-				p1[idx] += nums[idx]
+
+# PART 2
+p2 = 0
+nums = []
+N = []
+
+for line in inp:
+	N.append([c for c in line])
+
+ROWS = len(N)
+COLS = len(N[0])
+nums = []
+
+for c in range(COLS - 1, -1, -1):
+	tmp = ""
+	for r in range(ROWS):
+		a = N[r][c]
+		if r == ROWS - 1 and len(tmp) != 0:
+			nums.append(tmp)
+		if a == " " or a == "": continue
+		if a == "*":
+			mm = reduce(lambda x,y: int(x) * int(y), nums)
+			p2 += mm
+			nums =[]
+			continue
+		if a == "+":
+			ss = sum(int(i) for i in nums)
+			p2 += ss
+			nums = []
+			continue
+		tmp += a
+
+print(p2)
